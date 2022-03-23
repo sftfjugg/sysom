3ae075feb070605737a948510fd0691d6b62c8b3
wb-msm261421 <wb-msm261421@alibaba-inc.com>
2022-03-23 17:32:38 +0800

    * tasks 获取svg

--- sysom_api/apps/task/urls.py
+++ sysom_api/apps/task/urls.py
@@ -5,7 +5,7 @@
 @Author  : DM
 @Software: PyCharm
 """
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from apps.task import views
 
@@ -16,4 +16,5 @@ router.register('tasks', views.TaskAPIView)
 
 urlpatterns = [
     path('api/v1/', include(router.urls)),
+    re_path('^api/v1/tasks/(?P<task_id>\d+)/(?P<etx>[a-zA-Z]+)/$', views.TaskAPIView.as_view({'get': 'get_task_svg'})),
 ]
