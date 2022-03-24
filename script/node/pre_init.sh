43b1e747183ad22715d8e24df2d178a9fc5fcd6c
huangtuq <tuquanhuang@linux.alibaba.com>
2022-03-24 20:33:24 +0800

    add sub dir APP_HOME/node for node machine
    Signed-off-by: huangtuq <tuquanhuang@linux.alibaba.com>

--- script/node/pre_init.sh
+++ script/node/pre_init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 
+NODE_HOME=${APP_HOME}/node
 UPLOAD_DIR=${APP_HOME}/target/sysom_web/download/
 APP_CMD_CONF=${APP_HOME}/target/sysom_api/conf/product.py
 NODE_INIT_SCRIPT=${APP_HOME}/target/sysom_api/service_scripts/node_init
@@ -23,6 +24,7 @@ prepare_init_tar()
 {
     rm -f conf
     echo "APP_HOME=${APP_HOME}" >> conf
+    echo "NODE_HOME=${NODE_HOME}" >> conf
     echo "SERVER_LOCAL_IP=${SERVER_LOCAL_IP}" >> conf
     echo "SERVER_PUBLIC_IP=${SERVER_PUBLIC_IP}" >> conf
     mkdir -p ../${NODE_INIT_DIR}
@@ -38,7 +40,9 @@ set_node_init_cmd()
     sed "s#server_local_ip=xxx#server_local_ip=\"${SERVER_LOCAL_IP}\"#" -i ${NODE_INIT_SCRIPT}
     sed "s#server_public_ip=xxx#server_public_ip=\"${SERVER_PUBLIC_IP}\"#" -i  ${NODE_INIT_SCRIPT}
     sed "s#app_home=xxx#app_home=\"${APP_HOME}\"#" -i ${NODE_INIT_SCRIPT}
+    sed "s#node_home=xxx#node_home=\"${NODE_HOME}\"#" -i ${NODE_INIT_SCRIPT}
     sed "s#app_home=xxx#app_home=\"${APP_HOME}\"#" -i ${NODE_DELETE_SCRIPT}
+    sed "s#node_home=xxx#node_home=\"${NODE_HOME}\"#" -i ${NODE_DELETE_SCRIPT}
 
 }
 
