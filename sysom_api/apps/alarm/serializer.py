70d6af00d11f078bf838e534d8a29b1d3a2d3064
剑子仙机 <lxp.1987@163.com>
2022-04-29 02:28:53 +0800

    告警中心后端代码实现

--- sysom_api/apps/alarm/serializer.py
+++ sysom_api/apps/alarm/serializer.py
@@ -1,12 +1,14 @@
 import logging
 from rest_framework import serializers
-from apps.alarm.models import AlarmModel
+from apps.alarm.models import AlarmModel, SubscribeModel
 logger = logging.getLogger(__name__)
 
 
 class AlarmSerializer(serializers.ModelSerializer):
     collected_time = serializers.DateTimeField(label=u'告警采集时间', format="%Y-%m-%d %H:%M:%S")
     level = serializers.SerializerMethodField(label='级别', read_only=True)
+    noticelcon_type = serializers.SerializerMethodField(label='通知类型')
+
     class Meta:
         model = AlarmModel
         fields = "__all__"
@@ -14,3 +16,18 @@ class AlarmSerializer(serializers.ModelSerializer):
     def get_level(self, obj):
         return obj.get_level_display()
 
+    def get_noticelcon_type(self, obj):
+        return obj.get_noticelcon_type_display()
+
+
+class AddAlarmSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = AlarmModel
+        fields = '__all__'
+
+class SubSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = SubscribeModel
+        fields = "__all__"
--- sysom_api/consumer/routing.py
+++ sysom_api/consumer/routing.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from channels.routing import URLRouter
-from .consumers import SshConsumer
+from .consumers import SshConsumer, NoticelconConsumer
 from .middleware import AuthMiddleware
 
 ws_router = AuthMiddleware(
     URLRouter([
         path('ws/ssh/', SshConsumer.as_asgi()),
+        path('ws/noticelcon/', NoticelconConsumer.as_asgi()),
     ])
 )
