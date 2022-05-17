a1c163e037dee802eeed8873d14cd8ce00f985bd
道曲 <xianpeng.lxp@alibaba-inc.com>
2022-05-17 17:40:51 +0800

    修复订阅表数据缺失导致主机添加失败的bug

--- tools/deploy/deploy.sh
+++ tools/deploy/deploy.sh
@@ -142,6 +142,7 @@ init_conf() {
     python manage.py makemigrations vul
     python manage.py migrate
     python manage.py loaddata ./apps/accounts/user.json
+    python manage.py loaddata ./apps/alarm/subscribe.json
     python manage.py loaddata ./apps/vmcore/vmcore.json
     popd
 }
