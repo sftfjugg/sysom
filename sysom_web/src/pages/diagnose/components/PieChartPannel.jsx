e3d9b2c63a70cc27612dad1cf81c8490b5e48dd5
fengfuqiu <fengfuqiu@linux.alibaba.com>
2022-07-06 00:36:33 +0800

    add piechart pannel type.

---
+++ sysom_web/src/pages/diagnose/components/PieChartPannel.jsx
@@ -0,0 +1,35 @@
+import ProCard from '@ant-design/pro-card';
+import { Pie } from '@ant-design/plots';
+
+const PieChartPannel = (props) => {
+  const configs = props.configs
+  const data = props.data
+
+  const pieTemplate = {
+    appendPadding: 10,
+    data: data,
+    angleField: '__value__',
+    colorField: 'id',
+    radius: 0.8,
+    label: {
+      type: 'outer',
+      content: '{name} {percentage}'
+    },
+    interactions: [
+      {
+        type: 'pie-legend-active',
+      },
+      {
+        type: 'element-active',
+      },
+    ],
+  };
+
+  return (
+    <ProCard title={configs.title} style={{height:300}}>
+      <Pie {...pieTemplate} />
+    </ProCard>
+  )
+}
+
+export default PieChartPannel
