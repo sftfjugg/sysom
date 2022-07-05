c8c01e3f72a2f723029c80b9a0ab61cdcd03887a
fengfuqiu <fengfuqiu@linux.alibaba.com>
2022-07-06 00:28:40 +0800

    Add new diagnose dashboard auto render framework Dashboard will dynamic rander by json。

---
+++ sysom_web/src/pages/diagnose/components/StatisticPannel.jsx
@@ -0,0 +1,29 @@
+import ProCard from '@ant-design/pro-card';
+import { Statistic} from 'antd';
+import fieldModifier from './fieldModifier'
+
+const StatisticPannel = (props) => {
+    //console.log("configs", props.configs.title)
+    //console.log(props)
+    const configs = props.configs
+    const datas = props.data  
+    
+    return (
+      <ProCard.Group title={configs.title} direction='row' bordered>
+        {
+          Object.keys(datas).map((key) => {
+            let data = datas[key]
+            let [value, color] = fieldModifier(props.configs?.fieldConfig, data.__value__, data, datas)
+  
+            return (
+              <ProCard key={key}>
+                <Statistic title={datas[key].key} value={value} valueStyle={{ color: color }} />
+              </ProCard>
+            )
+          })
+        }
+      </ProCard.Group>
+    )
+  }
+
+  export default StatisticPannel
