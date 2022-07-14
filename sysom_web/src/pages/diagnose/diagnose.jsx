de691d01a09b105527674a98fa099decf231e06a
fengfuqiu <fengfuqiu@linux.alibaba.com>
2022-07-14 22:47:00 +0800

    update memgraph_post & delect the compatibility code;

--- sysom_web/src/pages/diagnose/diagnose.jsx
+++ sysom_web/src/pages/diagnose/diagnose.jsx
@@ -23,46 +23,12 @@ const Diagnose = (props) => {
         })
     }, [])
 
-
-    const changeOldFormat = (msg) => {
-        let datasources = {}
-        datasources.id = msg.id
-        datasources.created_at = msg.created_at
-        datasources.task_id = msg.task_id
-        datasources.service_name = msg.service_name
-        datasources.dataMemEvent = {
-            "data": [
-                { "key": "内存利用率", "value": parseFloat(msg.result.event.util.toFixed(2)) },
-                { "key": "内存泄漏检查", "value": msg.result.event.leak ? "正常" : "危险" },
-                { "key": "Memcg泄漏检查", "value": msg.result.event.memcg ? "正常" : "危险" },
-                { "key": "内存碎片化评估", "value": msg.result.event.memfrag ? "正常" : "危险" }
-            ]
-        }
-        _.set(datasources, 'dataMemOverView.data',
-            Object.entries(msg.result.memgraph).map(item => ({ key: item[0], value: item[1] })))
-        _.set(datasources, 'dataKerMem.data',
-            Object.entries(msg.result.kernel).map(item => ({ key: item[0], value: item[1] })))
-        _.set(datasources, 'dataUserMem.data',
-            Object.entries(msg.result.user).map(item => ({ key: item[0], value: item[1] })))
-        _.set(datasources, 'dataCacheList.data', msg.result.filecacheTop.map(
-            (item, i) => ({ key: i, 文件名: item.file, cached: item.cached, "进程": item.task.join(',') })))
-        _.set(datasources, 'dataProcMemList.data', msg.result.taskMemTop.map(
-            (item, i) => ({ key: i, 进程名: item.comm, 总内存: item.total_mem, RssAnon: item.RssAnon, RssFile: item.RssFile })))
-
-        console.log("datasources", datasources)
-        return datasources
-    }
-
     const onListClick = async (record) => {
         const recorded = record;
         const msg = await _getTask(record.id);
 
-        //目前仅仅支持内存大屏的数据格式旧数据格式转换，其他使用场合请直接使用新数据格式
-        if (msg.params.service_name == "memgraph" && msg.result.event) {
-            setData(changeOldFormat(msg));
-        } else {
-            setData({ ...msg, ...msg.result });
-        }
+        setData({ ...msg, ...msg.result });
+
     }
 
     const onPostTask = () => {
