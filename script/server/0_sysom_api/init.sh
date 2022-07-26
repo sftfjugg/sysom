e24d97d8c3bc4b51476f295e477364460ae474a2
huangtuq <tuquanhuang@linux.alibaba.com>
2022-07-26 11:59:08 +0800

    move yum install pkg into deploy.sh
    del some unuse comment codes
    
    Signed-off-by: huangtuq <tuquanhuang@linux.alibaba.com>

--- script/server/0_sysom_api/init.sh
+++ script/server/0_sysom_api/init.sh
@@ -22,24 +22,6 @@ fi
 
 mkdir -p ${SERVER_HOME}
 
-touch_env_rpms() {
-    if [ -f /etc/alios-release ]; then
-        if [ ! -f /etc/yum.repos.d/epel.repo ]; then
-            wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
-        fi
-    elif [ -f /etc/anolis-release ]; then
-        sed -i '/epel\/8\/Everything/{n;s/enabled=0/enabled=1/;}' /etc/yum.repos.d/AnolisOS-DDE.repo
-    fi
-    rpm -q --quiet python3 || yum install -y python3
-    rpm -q --quiet python3-virtualenv || yum install -y python3-virtualenv
-    rpm -q --quiet mariadb-server || yum install -y mariadb-server
-    rpm -q --quiet supervisor || yum install -y supervisor
-    rpm -q --quiet nginx || yum install -y nginx
-    rpm -q --quiet gcc || yum install -y gcc
-    rpm -q --quiet make || yum install -y make
-    rpm -q --quiet redis || yum install -y redis
-}
-
 touch_virtualenv() {
     mkdir -p ~/.pip
     cp pip.conf ~/.pip/
@@ -58,17 +40,6 @@ touch_virtualenv() {
     source ${VIRTUALENV_HOME}/bin/activate || exit 1
 }
 
-
-update_target() {
-    if [ -d "${TARGET_PATH}" ]; then
-        rm -rf ${TARGET_PATH}
-    fi
-    mkdir -p ${TARGET_PATH}
-    echo "INFO: copy project file..."
-    cp -r ${API_DIR} ${WEB_DIR} ${TARGET_PATH}
-
-}
-
 check_requirements() {
     echo "INFO: begin install requirements..."
 
@@ -79,7 +50,6 @@ check_requirements() {
     local requirements_log="${SERVER_HOME}/logs/${APP_NAME}_requirements.log"
     local requirements="requirements.txt"
     touch "$requirements_log" || exit
-    pip install pytest-runner cffi
     pip install -r ${requirements} -i "${ALIYUN_MIRROR}" |tee -a "${requirements_log}" || exit 1
     local pip_res=$?
     if [ $pip_res -ne 0 ]; then
@@ -130,9 +100,7 @@ start_app() {
 }
 
 deploy() {
-#    touch_env_rpms
     touch_virtualenv
-#    update_target
     check_requirements
     setup_database | tee -a ${SERVER_HOME}/logs/${APP_NAME}_setup_database.log 2>&1
     init_conf
