10feaef4cbe477e44feb66cdea844ba84803699b
huangtuq <tuquanhuang@linux.alibaba.com>
2022-08-05 11:03:20 +0800

    update node.json only when changes occur
    Signed-off-by: huangtuq <tuquanhuang@linux.alibaba.com>

--- script/server/1_monitor/monitor_server_deploy.sh
+++ script/server/1_monitor/monitor_server_deploy.sh
@@ -173,12 +173,8 @@ configure_grafana()
 
 configure_cron()
 {
-    echo "* * * * * python3 $RESOURCE_DIR/prometheus/prometheus_get_node.py" ${SERVER_HOME} >> /var/spool/cron/root
-    echo "* * * * * sleep 10;python3 $RESOURCE_DIR/prometheus/prometheus_get_node.py" ${SERVER_HOME} >> /var/spool/cron/root
-    echo "* * * * * sleep 20;python3 $RESOURCE_DIR/prometheus/prometheus_get_node.py" ${SERVER_HOME} >> /var/spool/cron/root
-    echo "* * * * * sleep 30;python3 $RESOURCE_DIR/prometheus/prometheus_get_node.py" ${SERVER_HOME} >> /var/spool/cron/root
-    echo "* * * * * sleep 40;python3 $RESOURCE_DIR/prometheus/prometheus_get_node.py" ${SERVER_HOME} >> /var/spool/cron/root
-    echo "* * * * * sleep 50;python3 $RESOURCE_DIR/prometheus/prometheus_get_node.py" ${SERVER_HOME} >> /var/spool/cron/root
+    echo "* * * * * python3 $RESOURCE_DIR/prometheus/prometheus_get_node.py" >> /var/spool/cron/root
+    echo "* * * * * sleep 30;python3 $RESOURCE_DIR/prometheus/prometheus_get_node.py" >> /var/spool/cron/root
 }
 
 main()
