d94833e45f766b912ce4aa72baedcd5470e728ea
huangtuq <tuquanhuang@linux.alibaba.com>
2022-08-05 11:03:20 +0800

    update node.json only when changes occur
    Signed-off-by: huangtuq <tuquanhuang@linux.alibaba.com>

--- script/server/1_monitor/prometheus_get_node.py
+++ script/server/1_monitor/prometheus_get_node.py
@@ -2,28 +2,47 @@
 import requests
 import json
 import sys
+import os
+import hashlib
+import shutil
 
-if len(sys.argv) < 2:
-    fname="monitor/prometheus/node/node.json"
-else:
-    fname=sys.argv[1]+"/monitor/prometheus/node/node.json"
+tfile=os.path.dirname(os.path.realpath(__file__))+"/node/node.json"
+#print(tfile)
 
+ofile="/tmp/sysom_node.json"
 host_api="http://localhost/api/v1/host"
 
 hostlist = requests.get(host_api)
 res = hostlist.content
 
-print(res)
+#print(res)
 hosts = json.loads(res)
 
 host_len = len(hosts["data"])
 iplist=[]
-fo = open(fname,"w")
+fo = open(ofile,"w")
 for i in range(0,host_len):
   iplist.append(hosts["data"][i]["ip"]+":9100")
 
 target={"targets":iplist}
 res="["+json.dumps(target)+"]"
-print(res)
+#print(res)
 fo.write(res)
 fo.close()
+
+if os.path.exists(tfile):
+  with open(ofile, 'rb') as fp:
+    data = fp.read()
+    fp.close()
+
+  amd5= hashlib.md5(data).hexdigest()
+
+  with open(tfile, 'rb') as fp2:
+    data2 = fp2.read()
+    fp2.close()
+
+  bmd5= hashlib.md5(data2).hexdigest()
+  if amd5 != bmd5:
+    shutil.move(ofile,tfile)
+else:
+  shutil.move(ofile,tfile)
