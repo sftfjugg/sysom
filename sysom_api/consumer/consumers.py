d4ffa144d5cdf5a683418112931d7626ad935993
SunnyQjm <mfeng@linux.alibaba.com>
2022-09-23 17:03:10 +0800

    add: use one thread to deal all notification task
    添加: 使用单个线程支持所有用户的推送工作,并且在没有用户接入时释放占用的资源

--- sysom_api/consumer/consumers.py
+++ sysom_api/consumer/consumers.py
@@ -1,15 +1,14 @@
 import logging
-import json
 from threading import Thread
+import threading
 from urllib import parse
 from channels.generic.websocket import WebsocketConsumer, JsonWebsocketConsumer
 from channels.exceptions import StopConsumer
 from django.conf import settings
-from django_redis import get_redis_connection
 import os
 from django.conf import settings
+from lib.utils import uuid_32
 from sdk.cec_base.consumer import Consumer, dispatch_consumer
-
 
 logger = logging.getLogger(__name__)
 
@@ -108,34 +107,120 @@ class SshConsumer(WebsocketConsumer):
         raise StopConsumer()
 
 
+class NotificationManager:
+    """A singleton class to manager user notification 
+
+    一个单例类实现，用于记录用户的 websocket 列表，并在通知产生时下发给对应的客户端
+    """
+    def __init__(self) -> None:
+        if not hasattr(NotificationManager, "_first_init"):
+            # 事件中心消费客户端，用于从事件中心获取要推送的通知消息
+            self._consumer: Consumer = None
+
+            # websocket 映射，记录每一个 username => [websocket] 的映射
+            self._ws_dict = {}
+            # 记录连接的数量
+            self._connection_count = 0
+            self._inner_locker = threading.Lock()
+            self._loop_message_thread: Thread = None
+            NotificationManager._first_init = True
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(NotificationManager, "_instance"):
+            NotificationManager._instance = object.__new__(cls)
+        return NotificationManager._instance
+
+    def _loop_message(self):
+        for message in self._consumer:
+            sub = message.value.get('sub', '')
+            if sub in self._ws_dict:
+                for _id in self._ws_dict[sub]:
+                    self._ws_dict[sub][_id].send_json(message.value)
+
+    def _start_consume_thread(self):
+        """Start consume thread
+
+        启动消费线程，处理消费
+        """
+        if self._loop_message_thread is not None \
+                and self._loop_message_thread.isAlive():
+            return
+        self._consumer = dispatch_consumer(
+            settings.SYSOM_CEC_URL, settings.SYSOM_CEC_ALARM_TOPIC,
+            consumer_id=Consumer.generate_consumer_id(), start_from_now=True
+        )
+        self._loop_message_thread = Thread(target=self._loop_message)
+        self._loop_message_thread.start()
+
+    def _stop_consume_thread(self):
+        """ Stop consume thread
+
+        停止消费线程
+        """
+        if self._loop_message_thread is not None \
+                and self._loop_message_thread.isAlive():
+            self._consumer.disconnect()
+            self._consumer = None
+            self._loop_message_thread.join()
+
+    def on_websocket_connect(self, username: str, nc: JsonWebsocketConsumer) -> str:
+        """invoke while a websocket connection establish successed
+
+        一个接受推送通知的 Websocket 建立成功之后，将会调用这个函数
+        """
+        _id = uuid_32()
+        with self._inner_locker:
+            if username not in self._ws_dict:
+                self._ws_dict[username] = {}
+            self._ws_dict[username][_id] = nc
+            self._connection_count += 1
+
+            # 如果之前没有客户端，第一个客户端连接之后就启动消费线程
+            if self._connection_count == 1:
+                self._start_consume_thread()
+        return _id
+
+    def on_websocket_disconnect(self, username: str, _id: str):
+        """invoke while a websocket connection disconnected
+
+        一个接受推送通知的 Websocket 断开连接之后，将会调用这个函数
+        """
+        with self._inner_locker:
+            if username in self._ws_dict:
+                if self._ws_dict[username].pop(_id, None) is not None:
+                    self._connection_count -= 1
+
+                    # 如果客户端清空了，就停止消费线程
+                    if self._connection_count == 0:
+                        self._stop_consume_thread()
+
+
 class NoticelconConsumer(JsonWebsocketConsumer):
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._user = None
-        self.consumer = None
-        self._loop_message_thread: Thread = None
+        self._id = ""
+        self._notification_manager = NotificationManager()
 
     def connect(self):
         self._user = self.scope['user']
         if self._user:
             self.accept()
-            self.consumer = dispatch_consumer(settings.SYSOM_CEC_URL, settings.SYSOM_CEC_ALARM_TOPIC,
-                                              consumer_id=Consumer.generate_consumer_id(), start_from_now=True)
-            self._loop_message_thread = Thread(target=self.loop_message)
-            self._loop_message_thread.start()
+            self._id = self._notification_manager.on_websocket_connect(
+                self._user.username,
+                self
+            )
         else:
             self.close()
-    
-    def loop_message(self):
-        for message in self.consumer:
-            if message.value.get('sub', '') == self._user.username:
-                self.send_json(message.value)
 
     def disconnect(self, code):
         """
         Websocket 断开连接时，断开与事件中心的连接，并释放线程资源
         """
-        self.consumer.disconnect()
-        if self._loop_message_thread is not None and self._loop_message_thread.isAlive():
-            self._loop_message_thread.join()
+        if self._user is not None:
+            self._notification_manager.on_websocket_disconnect(
+                self._user.username,
+                self._id
+            )
         return super().disconnect(code)
