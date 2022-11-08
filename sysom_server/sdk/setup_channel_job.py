27ebe92a0607be6e65fafd31c4ba96fce27722d7
SunnyQjm <mfeng@linux.alibaba.com>
2022-11-08 17:11:29 +0800

    feat(sdk): Add channel_job to support channel interaction

---
+++ sysom_server/sdk/setup_channel_job.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="channel_job",
+    version="0.0.1",
+    author="mingfeng(SunnyQjm)",
+    author_email="mfeng@linux.alibaba.com",
+    description="Used to implement synchronous asynchronous communication with the Channel module",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL License",
+        "Operating System :: OS Independent",
+    ]
+)
