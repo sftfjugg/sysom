e74f6ac5d3866634ce97f287b9f48a6961c2ec57
huangtuq <tuquanhuang@linux.alibaba.com>
2022-12-01 10:47:29 +0800

    bugfix of stop and clear vmcore scripts
    Signed-off-by: huangtuq <tuquanhuang@linux.alibaba.com>

--- script/server/2_vmcore/clear.sh
+++ script/server/2_vmcore/clear.sh
@@ -1,7 +1,13 @@
-#! /bin/sh
-sed -i '/vmcore/d' /var/spool/cron/root
-sed -i '/vmcore/d' /etc/exports
-exportfs -rv
-systemctl stop nfs-server
-systemctl stop rpcbind
-systemctl stop nfs
+#!/bin/bash
+SERVICE_NAME=sysom-channel
+stop_app() {
+    sed -i '/vmcore/d' /var/spool/cron/root
+    sed -i '/vmcore/d' /etc/exports
+    exportfs -rv
+    systemctl stop nfs-server
+    systemctl stop rpcbind
+    systemctl stop nfs
+    supervisorctl stop $SERVICE_NAME
+}
+
+stop_app
