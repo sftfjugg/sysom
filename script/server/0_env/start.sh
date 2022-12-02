c335a85b1705218903e9c0a104583473290308ad
huangtuq <tuquanhuang@linux.alibaba.com>
2022-12-02 14:27:11 +0800

    update local service scripts
    Signed-off-by: huangtuq <tuquanhuang@linux.alibaba.com>

--- script/server/0_env/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-start_app() {
-    systemctl start mariadb.service
-    systemctl start nginx.service
-    systemctl start redis.service
-    systemctl start supervisord.service
-}
-
-start_app
