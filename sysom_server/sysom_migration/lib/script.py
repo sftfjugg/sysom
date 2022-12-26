6c17751c70114e1197f4beb415add9677a5582ef
温凊 <wenqing.ly@alibaba-inc.com>
2022-12-26 17:47:06 +0800

    fix: 调整实施工具安装

--- sysom_server/sysom_migration/lib/script.py
+++ sysom_server/sysom_migration/lib/script.py
@@ -1,6 +1,9 @@
 ass_imp_script = '''
 tar -zxvf /tmp/ance/database/anolis_migration_pkgs.tar.gz -C /tmp/ance/database
-cd /tmp/ance/database/pkgs && yum install -y *.rpm rear genisoimage syslinux nfs-utils python3 wget
+cd /tmp/ance/database/pkgs
+yum install -y python-pip
+yum remove -y python-requests python-urllib3; /usr/bin/pip2 uninstall requests urllib3 -y 2>/dev/null || echo "not installed"
+yum install -y *.rpm rear genisoimage syslinux nfs-utils python3 wget
 REPO_URL
 leapp preupgrade --no-rhsm
 '''
@@ -31,7 +34,10 @@ echo soft_info:操作系统版本=$(cat /etc/os-release | grep '^PRETTY_NAME=' |
 
 deploy_tools_script = '''
 tar -zxvf /tmp/ance/database/anolis_migration_pkgs.tar.gz -C /tmp/ance/database
-cd /tmp/ance/database/pkgs && yum install -y *.rpm rear genisoimage syslinux nfs-utils python3 wget
+cd /tmp/ance/database/pkgs
+yum install -y python-pip
+yum remove -y python-requests python-urllib3; /usr/bin/pip2 uninstall requests urllib3 -y 2>/dev/null || echo "not installed"
+yum install -y *.rpm rear genisoimage syslinux nfs-utils python3 wget
 REPO_URL
 '''
 
