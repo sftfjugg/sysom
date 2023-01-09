62512cef7268fc0f18eecf2bf0f88a36697dc76a
ydzhang <ydzhang@linux.alibaba.com>
2023-01-09 17:09:15 +0800

    upload component of apps and fix the worong upload of base_model.py

--- sysom_server/sysom_hotfix/lib/base_model.py
+++ sysom_server/sysom_hotfix/lib/base_model.py
@@ -1,50 +1,20 @@
-import logging
-import os
-from typing import List
-from django.conf import settings
-from django.utils.translation import ugettext as _
-from rest_framework.exceptions import AuthenticationFailed
-from rest_framework.request import Request
-from rest_framework.authentication import BaseAuthentication
-from .utils import import_module
+# -*- encoding: utf-8 -*-
+"""
+@File    : base_model.py
+@Time    : 2021/10/28 11:04
+@Author  : DM
+@Email   : smmic@isoftstone.com
+@Software: PyCharm
+"""
+
+from django.db import models
+from lib.utils import human_datetime
 
 
-logger = logging.getLogger(__name__)
+class BaseModel(models.Model):
+    """abstract model"""
+    created_at = models.CharField(max_length=20, default=human_datetime, verbose_name="创建时间")
+    deleted_at = models.CharField(max_length=20, null=True)
 
-
-def get_jwt_decode_classes() -> List[BaseAuthentication]:
-    jwt_decode_classes = []
-    import_strings = [
-        f'lib.decode.{f.replace(".py", "")}' for f in os.listdir(settings.JWT_TOKEN_DECODE_DIR)
-    ]
-    for string in import_strings:
-        module = import_module(string)
-        try:
-            m = getattr(module, 'JWTTokenDecode')
-            jwt_decode_classes.append(m)
-        except Exception as exc:
-            logger.warn(exc)
-    return jwt_decode_classes
-
-
-def decode_token(token: str) -> dict:
-    error_message, success, result = "", False, {}
-    for auth_class in get_jwt_decode_classes():
-        result, success = auth_class().decode(token)
-        if not success:
-            error_message += result
-        else:
-            break
-    if not success:
-        raise AuthenticationFailed(error_message)
-    return result
-
-
-class TaskAuthentication(BaseAuthentication):
-    def authenticate(self, request: Request):
-        token = request.META.get('HTTP_AUTHORIZATION')
-        payload = decode_token(token)
-        payload['token'] = token
-        if 'sub' in payload:
-            payload['id'] = int(payload['sub'])
-        return payload, _
+    class Meta:
+        abstract = True
