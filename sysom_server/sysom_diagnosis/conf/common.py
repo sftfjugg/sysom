1220b9cb148ca94615e6796fea3161c3fe35cafb
Stick_to_yourself <1422737964@qq.com>
2023-01-11 17:54:28 +0800

    [需求: sysom_diagnosis.authorization] check whether the user is logout

--- sysom_server/sysom_diagnosis/conf/common.py
+++ sysom_server/sysom_diagnosis/conf/common.py
@@ -47,6 +47,18 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {"max_connections": 100},
+            "DECODE_RESPONSES": True
+        }
+    }
+}
+
 ROOT_URLCONF = 'sysom_diagnosis.urls'
 
 WSGI_APPLICATION = 'sysom_diagnosis.wsgi.application'
