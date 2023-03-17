f5eba606e9e8e2276be9595be45248dbabcb95c7
DM <1422737964@qq.com>
2023-03-17 11:29:09 +0800

    Fix[sysom-diagnosis]: 禁用gunicorn使用async模式适配Python3.9

--- sysom_server/sysom_diagnosis/conf/diagnosis_gunicorn.py
+++ sysom_server/sysom_diagnosis/conf/diagnosis_gunicorn.py
@@ -7,7 +7,7 @@ threads = 3
 
 bind = '127.0.0.1:7002'
 
-worker_class = 'gevent'  # 工作模式线程, 默认为sync模式
+# worker_class = 'gevent'  # 工作模式线程, 默认为sync模式
 
 max_requests = 2000      # 设置最大并发数量为2000 (每个worker处理请求的工作线程)
 
